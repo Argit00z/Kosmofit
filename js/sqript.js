f13561755b6c9b9641ff66bcc67e6d25b22d1ef8
Arman Galstyan <galstyanarman05@gmail.com>
2024-07-28 20:31:54 +0300

    add new slider

--- js/sqript.js
+++ js/sqript.js
@@ -1,31 +1,88 @@
-const images = document.querySelectorAll('.slider-img');
-const controlls = document.querySelectorAll('.controlls');
-let imageIndex = 0;
+const images = ['ofp_b.jpg', 'trenajer.jpg', 'logo.jpg'];
 
-function show(index) {
-    images[imageIndex].classList.remove('active');
-    images[index].classList.add('active');
-    imageIndex = index;
-}
+let activeImage = 0;
+const sliderPlace = document.querySelector('.slider-line');
+const widthOffset = document.querySelector('.slider').offsetWidth;
+sliderPlace.style.width = 3 * widthOffset + 'px';
+sliderPlace.style.height = widthOffset + 'px';
+sliderPlace.style.left = '-' + widthOffset + 'px';
+let flag = true;
 
-controlls.forEach((e) => {
-    e.addEventListener('click', () => {
-        if (event.target.classList.contains('prev')) {
-            let index = imageIndex - 1;
+const initSlider = () => {
+    const img = document.createElement('img');
+    img.alt = '';
+    img.src = './photo/' + images[activeImage];
+    sliderPlace.append(img);
+    nextImageGenerator();
+    prevImageGenerator();
+    
+};
 
-            if (index < 0) {
-                index = images.length - 1;
-            }
+const nextImageGenerator = () => {
+    let nextImage = activeImage + 1;
+    if (nextImage >= images.length) {
+        nextImage = 0;
+    } 
+    const img = document.createElement('img');
+    img.alt = '';
+    img.src = './photo/' + images[nextImage];
+    sliderPlace.append(img);
+};
+const prevImageGenerator = () => {
+    let prevImage = activeImage - 1;
+    if (prevImage < 0) {
+        prevImage = images.length - 1;
+    }
+    const img = document.createElement('img');
+    img.alt = '';
+    img.src = './photo/' + images[prevImage];
+    sliderPlace.prepend(img);
+};
+const nextSlide = () => {
+    activeImage++;
+    if (activeImage >= images.length) {
+        activeImage = 0;
+    }
+    //document.querySelector('.slider-line img').remove();
+    nextImageGenerator();
+    animate({
+        duration: 1000,
+        draw: function(progress) {
+            document.querySelector('.slider-line img').style.width = (widthOffset * (1 - progress)) + 'px'
+        },
+        removeElement: document.querySelector('.slider-line img')
+    });
 
-            show(index);
-        } else if (event.target.classList.contains('nextt')) {
-            let index = imageIndex + 1;
-            if (index >= images.length) {
-                index = 0;
-            }
-            show(index);
+};
+
+const prevSlide = () => {
+    activeImage--;
+    if (activeImage < 0) {
+        activeImage = images.length - 1;
+    }
+    document.querySelector('.slider-line img:last-child').remove();
+    prevImageGenerator();
+
+};
+
+initSlider();
+
+document.querySelector('.next-button').addEventListener('click', nextSlide);
+document.querySelector('.prev-button').addEventListener('click', prevSlide);
+
+const animate = ({duration, draw, removeElement}) => {
+    const start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+        let step = (time - start) / duration;
+        if (step > 1) {
+            step = 1;
         }
-    })
-})
-
-show(imageIndex);
+        draw(step);
+        if (step < 1){
+            requestAnimationFrame(animate);
+        } else {
+            removeElement.remove();
+        }   
+    });
+}
