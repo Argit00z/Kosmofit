760c3f1873d2af519208e3e1f19d4a072e339ba0
Arman <galstyanarman05@gmail.com>
2024-07-30 10:54:52 +0300

    fix slider

--- js/sqript.js
+++ js/sqript.js
@@ -1,88 +1,116 @@
-const images = ['ofp_b.jpg', 'trenajer.jpg', 'logo.jpg'];
+const images = ['trenajer.jpg', 'ofp_b.jpg' ];
 
 let activeImage = 0;
 const sliderPlace = document.querySelector('.slider-line');
-const widthOffset = document.querySelector('.slider').offsetWidth;
-sliderPlace.style.width = 3 * widthOffset + 'px';
-sliderPlace.style.height = widthOffset + 'px';
-sliderPlace.style.left = '-' + widthOffset + 'px';
+const widthOffset = document.querySelector('.slider').clientWidth;
+sliderPlace.style.width = 3*widthOffset + 'px';
+sliderPlace.style.heigth = widthOffset + 'px';
+document.querySelector('.slider').style.heigth = widthOffset + 'px';
 let flag = true;
+
+const firstText = document.querySelector
+
 
 const initSlider = () => {
     const img = document.createElement('img');
     img.alt = '';
     img.src = './photo/' + images[activeImage];
     sliderPlace.append(img);
-    nextImageGenerator();
-    prevImageGenerator();
-    
-};
+    nextImageGenerate();
+    prevImageGenerate();
+}
 
-const nextImageGenerator = () => {
+const nextImageGenerate = () => {
+    // проверим какая следующая картинка
     let nextImage = activeImage + 1;
     if (nextImage >= images.length) {
         nextImage = 0;
-    } 
+    }
     const img = document.createElement('img');
     img.alt = '';
     img.src = './photo/' + images[nextImage];
     sliderPlace.append(img);
-};
-const prevImageGenerator = () => {
+}
+
+const prevImageGenerate = (w = false) => {
+    // проверим какая следующая картинка
     let prevImage = activeImage - 1;
     if (prevImage < 0) {
         prevImage = images.length - 1;
     }
     const img = document.createElement('img');
     img.alt = '';
+    if (w) img.style.width = 0;
     img.src = './photo/' + images[prevImage];
     sliderPlace.prepend(img);
-};
+}
+
+initSlider();
+// itgid.info
+
 const nextSlide = () => {
+    if (!flag) return;
+    flag = !flag;
+    
     activeImage++;
     if (activeImage >= images.length) {
         activeImage = 0;
     }
-    //document.querySelector('.slider-line img').remove();
-    nextImageGenerator();
-    animate({
-        duration: 1000,
-        draw: function(progress) {
-            document.querySelector('.slider-line img').style.width = (widthOffset * (1 - progress)) + 'px'
-        },
-        removeElement: document.querySelector('.slider-line img')
-    });
+    nextImageGenerate();
 
-};
+    animate({
+        duration: 700,
+        draw: function(progress) {
+            document.querySelector('.slider-line > img').style.width = (widthOffset * (1- progress))+ 'px';
+        },
+        removeElement :     document.querySelector('.slider-line > img')    
+    });
+}
 
 const prevSlide = () => {
+    if (!flag) return;
+    flag = !flag;
+    
     activeImage--;
     if (activeImage < 0) {
         activeImage = images.length - 1;
     }
-    document.querySelector('.slider-line img:last-child').remove();
-    prevImageGenerator();
+    prevImageGenerate(true);
 
-};
+    animate({
+        duration: 700,
+        draw: function(progress) {
+            document.querySelector('.slider-line > img').style.width = (widthOffset * progress)+ 'px';
+        },
+        timing: function(step) {
+          return step;
+        },
+        removeElement :     document.querySelector('.slider-line > img:last-child')      
+      });
+ 
+}
 
-initSlider();
 
 document.querySelector('.next-button').addEventListener('click', nextSlide);
 document.querySelector('.prev-button').addEventListener('click', prevSlide);
 
-const animate = ({duration, draw, removeElement}) => {
-    const start = performance.now();
+function animate({duration, draw, removeElement}) {
 
+    let start = performance.now();
+  
     requestAnimationFrame(function animate(time) {
-        let step = (time - start) / duration;
-        if (step > 1) {
-            step = 1;
-        }
-        draw(step);
-        if (step < 1){
-            requestAnimationFrame(animate);
-        } else {
-            removeElement.remove();
-        }   
+      let step = (time - start) / duration;
+
+      if (step > 1) step = 1;
+  
+      draw(step);
+  
+      if (step < 1) {
+        requestAnimationFrame(animate);
+      }
+      else {
+        removeElement.remove();
+        flag = true;
+      }
     });
-}
+  }
